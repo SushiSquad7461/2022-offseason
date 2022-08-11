33ea9806658980d282a8e78abb7160624de690d8
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-08-11 13:35:56 -0700

    change to wpilib

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,34 @@
+package frc.robot;
+
+import SushiFrcLib.Motor.MotorConfig;
+import SushiFrcLib.Motor.SwerveModuleConfig;
+import SushiFrcLib.State.State.MotorDirection;
+import SushiFrcLib.State.State.MotorNeturalState;
+import SushiFrcLib.State.State.MotorType;
+
+public final class Constants{
+        public static final class kIntake {
+                public static final int PIVOT_CURRENT_LIMIT = 30;
+                public static final int ROLLER_CURRENT_LIMIT = 30;
+                public static final int EXTENDED_SETPOINT = 200;
+                public static final double kPPivot = 0;
+                public static final double kIPivot = 0;
+                public static final double kDPivot = 0;
+                public static final double kFPivot = 0;
+        }
+
+        public static class Ports {
+                public static final int INTAKE_PIVOT = -1;
+                public static final int INTAKE_ROLLER = -1;
+        }
+
+        public static class kSwerve {
+                public static final SwerveModuleConfig frontLeft = new SwerveModuleConfig(new MotorConfig(0, 0, MotorNeturalState.BRAKE, MotorDirection.CLOCKWISE, 0, 0, 0, 0, MotorType.BRUSHLESS), new MotorConfig(0, 0, MotorNeturalState.BRAKE, MotorDirection.CLOCKWISE, 0, 0, 0, 0, MotorType.BRUSHLESS), 0, 0);
+                public static final SwerveModuleConfig frontRight = new SwerveModuleConfig(new MotorConfig(0, 0, MotorNeturalState.BRAKE, MotorDirection.CLOCKWISE, 0, 0, 0, 0, MotorType.BRUSHLESS), new MotorConfig(0, 0, MotorNeturalState.BRAKE, MotorDirection.CLOCKWISE, 0, 0, 0, 0, MotorType.BRUSHLESS), 0, 0);
+                public static final SwerveModuleConfig backLeft = new SwerveModuleConfig(new MotorConfig(0, 0, MotorNeturalState.BRAKE, MotorDirection.CLOCKWISE, 0, 0, 0, 0, MotorType.BRUSHLESS), new MotorConfig(0, 0, MotorNeturalState.BRAKE, MotorDirection.CLOCKWISE, 0, 0, 0, 0, MotorType.BRUSHLESS), 0, 0);
+                public static final SwerveModuleConfig backRight = new SwerveModuleConfig(new MotorConfig(0, 0, MotorNeturalState.BRAKE, MotorDirection.CLOCKWISE, 0, 0, 0, 0, MotorType.BRUSHLESS), new MotorConfig(0, 0, MotorNeturalState.BRAKE, MotorDirection.CLOCKWISE, 0, 0, 0, 0, MotorType.BRUSHLESS), 0, 0);
+        }
+
+        public static void setup() {
+        }
+}
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,54 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import SushiFrcLib.Motor.MotorHelper;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Ports;
+import frc.robot.Constants.kIntake;
+
+public class Intake extends SubsystemBase {
+    // Motors
+    private final WPI_TalonFX pivot;
+    private final WPI_TalonFX roller;
+
+    //Subsystem Creation
+    private static Intake sInstance = null;
+
+    public static Intake getInstance(String caller) {
+        if (sInstance == null) {
+            sInstance = new Intake(caller);
+        }
+        return sInstance;
+    }
+
+    private Intake(String caller) {
+        pivot = MotorHelper.createFalconMotor(Ports.INTAKE_PIVOT, kIntake.PIVOT_CURRENT_LIMIT, TalonFXInvertType.CounterClockwise, NeutralMode.Brake);
+        roller = MotorHelper.createFalconMotor(Ports.INTAKE_ROLLER, kIntake.ROLLER_CURRENT_LIMIT, TalonFXInvertType.Clockwise, NeutralMode.Coast);
+        pivot.setSelectedSensorPosition(0);
+    }
+
+    @Override
+    public void periodic() { 
+    }
+
+    public void extend() {
+        setMotors(kIntake.EXTENDED_SETPOINT, 1);
+    }
+
+    public void reverse() {
+        setMotors(kIntake.EXTENDED_SETPOINT, -1);
+    }
+
+    public void retract() {
+        setMotors(0, 0);
+    }
+
+    private void setMotors(double pivotGoal, double rollerSpeed) {
+        pivot.set(ControlMode.Position, pivotGoal);
+        roller.set(ControlMode.PercentOutput, rollerSpeed);
+    }
+}
