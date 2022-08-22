a1807d8a4e265a0735c78dc62f77d44a16b5ae37
Maanav Sikaria <maanavsikaria@gmail.com>
2022-08-22 14:03:35 -0700

    cleanup

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,15 +1,5 @@
 package frc.robot;
 
-import SushiFrcLib.Motor.MotorConfig;
-import SushiFrcLib.Motor.SwerveModuleConfig;
-import SushiFrcLib.State.State.MotorDirection;
-import SushiFrcLib.State.State.MotorNeturalState;
-import SushiFrcLib.State.State.MotorType;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 
 public final class Constants{
         public static class Ports {
