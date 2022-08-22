ecc6dce5b19d92fd41cd4c9aa7f7f42bf610daac
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-08-22 10:19:36 -0700

    remove constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,8 +13,6 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 
 public final class Constants{
         public static class Ports {
-                public static final int INTAKE_PIVOT = -1;
-                public static final int INTAKE_ROLLER = -1;
         }
 
         public static class kSwerve {
