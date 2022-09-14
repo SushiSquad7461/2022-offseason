12cac191880d538b50077016470aa7d1fe2dbb2b
loukylor <loukylor@gmail.com>
2022-09-13 17:53:23 -0700

    initial color sensor code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.TCS3200_ColorSensor;
+import frc.robot.subsystems.TCS3200_ColorSensor.ColorSelect;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,6 +19,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   XboxController driver = new XboxController(0);
+  
+  TCS3200_ColorSensor colorSensor = new TCS3200_ColorSensor(2, -1, -1, 1, 0, ColorSelect.BLUE); 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
---
+++ src/main/java/frc/robot/subsystems/TCS3200_ColorSensor.java
@@ -0,0 +1,192 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TCS3200_ColorSensor extends SubsystemBase {
+  // Output managment
+  private Counter m_counter;
+
+  // Configurable registers
+  private DigitalOutput m_s0;  
+  private DigitalOutput m_s1;  
+  private DigitalOutput m_s2;  
+  private DigitalOutput m_s3;  
+
+  private FreqScaling m_freqScaling;
+  private ColorSelect m_currColor;
+
+  public enum FreqScaling {
+    POWER_DOWN(0),
+    PERCENT2(1),
+    PERCENT20(2),
+    PERCENT100(3);
+
+    public final int val;
+
+    FreqScaling(int i) {
+      val = i;
+    }
+  }
+
+  public enum ColorSelect {
+    RED(0),
+    GREEN(1),
+    BLUE(2),
+    CLEAR(3);
+
+    public final int val;
+
+    ColorSelect(int i) {
+      val = i;
+    } 
+  }
+
+  private int m_red;
+  private int m_green;
+  private int m_blue;
+  private int m_clear;
+  
+  public TCS3200_ColorSensor(int out, int s0, int s1, int s2, int s3, ColorSelect startColor) {
+    m_counter = new Counter(new DigitalInput(out));
+
+    // To save DIO ports, let user not set some
+    if (s0 != -1) {
+      m_s0 = new DigitalOutput(s0);
+    }
+
+    if (s1 != -1) {
+      m_s1 = new DigitalOutput(s1);
+    }
+
+    if (s2 != -1) {
+      m_s2 = new DigitalOutput(s2);
+    }
+
+    if (s3 != -1) {
+      m_s3 = new DigitalOutput(s3);
+    }
+
+    setFreqScaling(FreqScaling.PERCENT100);
+    setOutputColor(startColor);
+  }
+
+  public void setFreqScaling(FreqScaling scale) {
+    m_freqScaling = scale;
+
+    if (m_s0 == null || m_s1 == null) {
+      return;
+    }
+
+    // Check the data sheet pg 2 for the truth table
+    // https://www.mouser.com/catalog/specsheets/tcs3200-e11.pdf
+    switch (scale) {
+      case POWER_DOWN:
+        m_s0.set(false);
+        m_s1.set(false);
+      case PERCENT2:
+        m_s0.set(false);
+        m_s1.set(true);
+      case PERCENT20:
+        m_s0.set(true);
+        m_s1.set(false);
+      case PERCENT100:
+        m_s0.set(true);
+        m_s1.set(true);
+    }
+  }
+
+  public FreqScaling getFreqScaling() {
+    return m_freqScaling;
+  }
+
+  public void setOutputColor(ColorSelect color) {
+    m_currColor = color;
+
+    if (m_s2 == null || m_s3 == null) {
+      return;
+    }
+
+    // Check the data sheet pg 2 for the truth table
+    // https://www.mouser.com/catalog/specsheets/tcs3200-e11.pdf
+    switch (color) {
+      case RED:
+        m_s2.set(false);
+        m_s3.set(false);
+        break;
+      case BLUE:
+        m_s2.set(false);
+        m_s3.set(true);
+        break;
+      case CLEAR:
+        m_s2.set(true);
+        m_s3.set(false);
+        break;
+      case GREEN:
+        m_s2.set(true);
+        m_s3.set(true);
+        break;
+    }
+  }
+
+  public ColorSelect getOutputColor() {
+    return m_currColor;
+  }
+
+  public int getGreen() {
+    return m_green;
+  }
+
+  public int getRed() {
+    return m_red;
+  }
+  
+  public int getBlue() {
+    return m_blue;
+  }
+
+  public int getClear() {
+    return m_clear;
+  }
+
+  @Override
+  public void periodic() {
+    m_red = 0;
+    m_blue = 0;
+    m_green = 0;
+    m_clear = 0;
+
+    int freq = m_counter.get();
+    switch (m_currColor) {
+      case RED:
+        m_red = freq;
+        break;
+      case GREEN:
+        m_green = freq;
+        break;
+      case BLUE:
+        m_blue = freq;
+        break;
+      case CLEAR:
+        m_clear = freq;
+        break;
+    } 
+
+    m_counter.reset();
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+      super.initSendable(builder);
+
+      builder.addStringProperty("Frequency Scaling", () -> this.getFreqScaling().toString(), null);
+      builder.addStringProperty("Current Color", () -> this.getOutputColor().toString(), null);
+      builder.addDoubleProperty("Red", this::getRed, null);
+      builder.addDoubleProperty("Green", this::getGreen, null);
+      builder.addDoubleProperty("Blue", this::getBlue, null);
+      builder.addDoubleProperty("Clear", this::getClear, null);
+  }
+}
