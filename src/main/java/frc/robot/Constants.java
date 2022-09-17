4000fb1f432531bbcdd31752817dcd0b85ba2d8b
titanium-47 <rsidharth53@gmail.com>
2022-09-17 14:41:51 -0700

    tune hood

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,19 +15,23 @@ public final class Constants {
                 public static final int CURRENT_LIMIT = 35;
                 public static final TalonFXInvertType INVERSION = TalonFXInvertType.Clockwise;
                 public static final NeutralMode NEUTRAL_MODE = NeutralMode.Brake;
-                public static final double kP = 0;
+                public static final double kP = 0.8;
                 public static final double kI = 0;
-                public static final double kD = 0;
-                public static final double kF = 0.0006;
+                public static final double kD = 0.1;
+                public static final double kF = 0;
                 public static final double maxPos = 180;
                 public static final InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> posMap = new InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble>();
 
                 static {
-                        // First val is disntance from goal (TODO: INSERT UNITS), Second val is hood pos in encoder tiks
+                        // First val is disntance from goal (TODO: INSERT UNITS), Second val is hood pos
+                        // in encoder tiks
                         posMap.put(new InterpolatingDouble(0.0), new InterpolatingDouble(0.0));
                 }
-        } 
+        }
 
         public static void setup() {
         }
+
+        public static final boolean TUNING_MODE = true;
+
 }
--- src/main/java/frc/robot/subsytems/Hood.java
+++ src/main/java/frc/robot/subsytems/Hood.java
@@ -5,7 +5,10 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import SushiFrcLib.CheesyLibUtil.InterpolatingDouble;
 import SushiFrcLib.Motor.MotorHelper;
+import SushiFrcLib.SmartDashboard.TunableNumber;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.Ports;
 import frc.robot.Constants.kHood;
 
@@ -13,8 +16,14 @@ public class Hood extends SubsystemBase {
     private final WPI_TalonFX motor;
     private double pos = 0;
 
+    private TunableNumber hoodP = new TunableNumber("Hood P", Constants.kHood.kP, Constants.TUNING_MODE);
+    private TunableNumber hoodI = new TunableNumber("Hood I", Constants.kHood.kI, Constants.TUNING_MODE);
+    private TunableNumber hoodD = new TunableNumber("Hood D", Constants.kHood.kD, Constants.TUNING_MODE);
+    private TunableNumber hoodF = new TunableNumber("Hood F", Constants.kHood.kF, Constants.TUNING_MODE);
+    private TunableNumber targetPos = new TunableNumber("targetPos", 0, Constants.TUNING_MODE);
+
     private static Hood instance;
-    
+
     public static Hood getInstance() {
         if (instance == null) {
             instance = new Hood();
@@ -23,13 +32,14 @@ public class Hood extends SubsystemBase {
     }
 
     private Hood() {
-        motor = MotorHelper.createFalconMotor(Ports.HOOD_MOTOR, kHood.CURRENT_LIMIT, kHood.INVERSION, kHood.NEUTRAL_MODE, kHood.kP, kHood.kI, kHood.kD, kHood.kF);
+        motor = MotorHelper.createFalconMotor(Ports.HOOD_MOTOR, kHood.CURRENT_LIMIT, kHood.INVERSION,
+                kHood.NEUTRAL_MODE, hoodP.get(), hoodI.get(), hoodD.get(), hoodF.get());
         motor.setSelectedSensorPosition(0);
     }
 
     public void setPos(double newPos) {
         // if (0 <= newPos && newPos <= kHood.maxPos) {
-        //     pos = newPos;
+        // pos = newPos;
         // }
         pos = newPos;
     }
@@ -40,8 +50,15 @@ public class Hood extends SubsystemBase {
 
     @Override
     public void periodic() {
-       motor.set(ControlMode.Position, pos);
-       System.out.println(motor.getSelectedSensorPosition());
+        motor.set(ControlMode.Position, pos == 0 ? targetPos.get() : pos);
+        SmartDashboard.putNumber("Hood Position", motor.getSelectedSensorPosition());
         // motor.set(ControlMode.PercentOutput, 0.4);
-    } 
+
+        if (hoodP.hasChanged()) {
+            motor.config_kP(0, hoodP.get());
+        }
+        if (hoodD.hasChanged()) {
+            motor.config_kD(0, hoodD.get());
+        }
+    }
 }
