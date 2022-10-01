96f81689a7697958f2cdcd353a1b34ada24fd1af
loukylor <loukylor@gmail.com>
2022-10-01 11:21:46 -0700

    add getShooting

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -198,6 +198,10 @@ public class Indexer extends SubsystemBase {
     }
   }
 
+  public boolean getShooting() {
+    return isShooting;
+  }
+
   public boolean lowerBeamBreakActuated() {
     return !lowerBeamBreak.get();
   }
